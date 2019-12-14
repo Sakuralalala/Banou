21d178f524d35b846323ca0e351bcf8b80df0d67
Minstreams <minstreams@qq.com>
2019-12-14 10:20:50 +0800

    TriggerZone Update

---
+++ Assets/Scripts/Props/TriggerZone.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 触发区域
+/// </summary>
+public class TriggerZone : MonoBehaviour
+{
+    public UnityEvent onEnter;
+    public UnityEvent onExit;
+    public bool triggerOnlyOnce = false; //只触发一次
+
+    private string GetID()
+    {
+        return UnityEngine.SceneManagement.SceneManager.GetActiveScene().name + transform.GetSiblingIndex();
+    }
+    private static List<string> triggerdZones = new List<string>();
+    [RuntimeInitializeOnLoadMethod]
+    private static void RegisterInit()
+    {
+        GameSystem.TheMatrix.onGameInitialize += Init;
+    }
+    private static void Init()
+    {
+        triggerdZones.Clear();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (triggerOnlyOnce && !triggerdZones.Contains(GetID()))
+            {
+                onEnter?.Invoke();
+                triggerdZones.Add(GetID());
+            }
+            else
+            {
+                onEnter?.Invoke();
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            onExit?.Invoke();
+        }
+    }
+}
