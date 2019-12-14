c14b950c26a89a152400c40ede4cd7b70f47ab63
Minstreams <minstreams@qq.com>
2019-12-14 08:57:51 +0800

    DontDestroyOnLoader

---
+++ Assets/Scripts/LittleComponents/DontDestroyOnLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 小组件，让挂载的游戏物体独立于场景，应该只用于只加载一次的场景如System场景中
+/// </summary>
+public class DontDestroyOnLoader : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
