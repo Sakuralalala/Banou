8543077de5e915563db3609e80e4daf1c6f7a3c0
Minstreams <minstreams@qq.com>
2019-12-14 10:00:10 +0800

    Update Debug Printer

---
+++ Assets/Scripts/LIstener/DebugPrinter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Listener/DebugPrinter")]
+public class DebugPrinter : MonoBehaviour
+{
+    public string text;
+    public void Print()
+    {
+        print(text);
+    }
+    public void PrintFloat(float input)
+    {
+        print(input);
+    }
+    public void PrintVector2(Vector2 input)
+    {
+        print(input);
+    }
+    public void PrintVector3(Vector3 input)
+    {
+        print(input);
+    }
+    public void PrintString(string input)
+    {
+        print(input);
+    }
+}
