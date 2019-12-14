c64dd1b5ae82a9f5a67c0e297b9fab5675bb209f
Minstreams <minstreams@qq.com>
2019-12-14 13:02:57 +0800

    玩偶菜单

---
+++ Assets/Scripts/Context/FloatThreadholdEvent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FloatThreadholdEvent : MonoBehaviour
+{
+    [SerializeField]
+    private float value;
+    public float target;
+    public float threadhold;
+    private bool underThreadhold = true;
+    [Range(0, 1)]
+    public float rate = 0.9f;
+
+    public FloatEvent updateOutput;
+    public UnityEvent onOverThreadhold;
+    public UnityEvent onBelowThreadhold;
+
+    public void SetTarget(float t)
+    {
+        target = t;
+    }
+
+    private void Update()
+    {
+        float t = 1.0f - Mathf.Pow(1.0f - rate, Time.deltaTime / Time.timeScale);
+        value = Mathf.Lerp(value, target, t);
+        updateOutput?.Invoke(value);
+        if (underThreadhold)
+        {
+            if (value > threadhold)
+            {
+                underThreadhold = false;
+                onOverThreadhold?.Invoke();
+            }
+        }
+        else
+        {
+            if (value < threadhold)
+            {
+                underThreadhold = true;
+                onBelowThreadhold?.Invoke();
+            }
+        }
+    }
+}
--- Assets/Scripts/Context/KeyCodeInputEvent.cs
+++ Assets/Scripts/Context/KeyCodeInputEvent.cs
@@ -7,11 +7,13 @@ using UnityEngine.Events;
 public class KeyCodeInputEvent : MonoBehaviour
 {
     public UnityEvent output;
+    public UnityEvent upOutput;
     public KeyCode input;
 
     private void Update()
     {
         if (Input.GetKeyDown(input)) output?.Invoke();
+        if (Input.GetKeyUp(input)) upOutput?.Invoke();
     }
 
 }
