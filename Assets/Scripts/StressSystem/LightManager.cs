17f3dbaed9f2744448cc93384377a27b3803dcbe
Sakuralalala <1209251178@qq.com>
2019-12-15 02:35:22 +0800

    打乱数组

--- Assets/Scripts/StressSystem/LightManager.cs
+++ Assets/Scripts/StressSystem/LightManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class LightManager : StressEffect
 {
@@ -11,7 +12,29 @@ public class LightManager : StressEffect
         lightControllers = FindObjectsOfType<LightController>() as LightController[];
 
         //TODO 打乱顺序
-
+        LightController[] lightTemp;
+        lightTemp = new LightController[lightControllers.Length];
+        for(int i = 0; i < lightTemp.Length; i++)
+        {
+            lightTemp[i] = lightControllers[i];
+        }
+        System.Random rand = new System.Random(DateTime.Now.Millisecond);
+        for(int i = 0; i < lightTemp.Length; i++)
+        {
+            int x, y;
+            LightController light;
+            x = rand.Next(0, lightTemp.Length);
+            do
+            {
+                y = rand.Next(0, lightTemp.Length);
+            } while (y == x);
+            light = lightTemp[x];
+            lightTemp[x] = lightTemp[y];
+            lightTemp[y] = light;
+        }
+        lightControllers = lightTemp;
+        //Over
+        
         foreach (LightController lc in lightControllers)
         {
             lc.GetComponentInChildren<ParticleSystem>()?.collision.SetPlane(0, transform);
